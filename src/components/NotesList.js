1044a1c40679962b63eda33cedee9256312e31bf
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-09-13 16:15:04 +0700

    fix bug

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -4,7 +4,7 @@ import NoteItem from './NoteItem';
 
 function NotesList({ notes, keyword }) {
   function EmptyMessage() {
-    if (window.location.pathname.includes('/notes/archives')) {
+    if (window.location.pathname.includes('/archives')) {
       return (
         <h1 className=' absolute  text-2xl font-semibold text-slate-300 text-center  flex justify-center rounded-lg h-[400px] items-center w-full p-4'>
           Tidak Ada Catatan yang Diarsipkan
